bd2ef1d2e1bd64c4527a41366d32c02eae1a697c
Bin Liu <binliu14qd@gmail.com>
2024-02-08 11:26:55 -0600

    Create __init__.py

---
+++ notebooks/__init__.py
@@ -0,0 +1,8 @@
+'''This is the __init__.py file for the notebooks package.
+
+Contributer: Bin Liu
+Created Date: 2024-02-07
+Last Updated: 2024-02-07
+'''
+
+from .path_utils import add_path_to_sys
